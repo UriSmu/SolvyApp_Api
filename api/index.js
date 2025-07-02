01f7b643633f7e0a67432a126e12b0d1c0ea1985
UriSmu <urismu07@gmail.com>
2025-07-02 20:09:29 -0300

    Vercel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
 import app from "../src/app.js";
-import { createServer } from "http";
-import { parse } from "url";
 
-export default function handler(req, res) {
-  const server = createServer(app);
-  server.emit("request", req, res);
-}
+export default app;
